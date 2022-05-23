9286fdcfbde6be067df53b0a8ca8851e7311c120
Alec <alemor98@hotmail.it>
2022-05-23 17:04:06 +0200

    Pagina principale con API axios

---
+++ Test Tecnico MVC + Web API/Controllers/Api/AgenziaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Test_Tecnico_MVC___Web_API.Models;
+using Test_Tecnico_MVC___Web_API.Data;
+
+
+namespace Test_Tecnico_MVC___Web_API.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AgenziaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<PacchettoViaggio> listaViaggi = new();
+
+            using (AgenziaContext db= new AgenziaContext())
+            {
+                listaViaggi = db.Viaggi.ToList<PacchettoViaggio>();
+            }
+            return Ok(listaViaggi);
+        }
+    }
+}
